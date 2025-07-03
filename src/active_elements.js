702262de98b97bcffad00421e9a6bcff0d7a54f9
frank <wuensch.frank@web.de>
2025-07-03 20:08:21 +0200

    feat: add styling for active element

---
+++ src/active_elements.js
@@ -0,0 +1,21 @@
+---
+---
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".link-title");
+    let baseurl = "{{site.baseurl}}";
+    const home = navLinks[0];
+    const homeLink = baseurl + home.pathname;
+    let actualLink = baseurl + window.location.pathname;
+    navLinks.forEach(function(navLink) {
+        if (navLink.pathname === actualLink) {
+            navLink.classList.add("active");
+        } else {
+            navLink.classList.remove("active");
+        }
+        console.log(homeLink);
+        console.log(actualLink);
+        if (actualLink === homeLink || actualLink === baseurl + "/") {
+            home.classList.add("active");
+        }
+    });
+});
