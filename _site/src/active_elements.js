69a82d9a2a5257394e3494ec3cf9b675edc9eadd
frank <wuensch.frank@web.de>
2025-07-04 10:10:08 +0200

    test: fix issues

--- _site/src/active_elements.js
+++ _site/src/active_elements.js
@@ -1,16 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll(".link-title");
-    let baseurl = "";
+    const baseurl = "/bike-blog-fw/";
     const home = navLinks[0];
-    const homeLink = baseurl + home.pathname;
-    let actualLink = baseurl + window.location.pathname;
+    const homeLink = home.pathname;
+    let actualLink = window.location.pathname;
+    console.log(baseurl, actualLink);
     navLinks.forEach((navLink) => {
         if (navLink.pathname === actualLink) {
             navLink.classList.add("active");
         } else {
             navLink.classList.remove("active");
         }
-        if (actualLink === homeLink || actualLink === baseurl) {
+        if (actualLink === homeLink) {
             home.classList.add("active");
         }
     });
--- src/active_elements.js
+++ src/active_elements.js
@@ -2,17 +2,18 @@
 ---
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll(".link-title");
-    let baseurl = "{{site.baseurl}}";
+    const baseurl = "{{site.baseurl}}";
     const home = navLinks[0];
-    const homeLink = baseurl + home.pathname;
-    let actualLink = baseurl + window.location.pathname;
+    const homeLink = home.pathname;
+    let actualLink = window.location.pathname;
+    console.log(baseurl, actualLink);
     navLinks.forEach((navLink) => {
         if (navLink.pathname === actualLink) {
             navLink.classList.add("active");
         } else {
             navLink.classList.remove("active");
         }
-        if (actualLink === homeLink || actualLink === baseurl) {
+        if (actualLink === homeLink) {
             home.classList.add("active");
         }
     });
